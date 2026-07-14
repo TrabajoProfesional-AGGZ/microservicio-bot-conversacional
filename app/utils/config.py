5ccfbeb06bcde4fde5aba3e45e04098f1a0d20f3
Felipe Santino Ascencio <felipesantinoascencio@gmail.com>
2026-07-14 00:28:13 +0000

    Terminada la última versión de base del bot conversacional

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Settings:
-    VERIFY_TOKEN: str = os.getenv("VERIFY_TOKEN", "sociounido_token_secreto_123")
+    VERIFY_TOKEN: str = os.getenv("VERIFY_TOKEN", "")
     ENV: str = os.getenv("ENV", "development")
-    WHATSAPP_TOKEN: str = os.getenv("WHATSAPP_TOKEN", "pega_tu_token_aqui")
-    WHATSAPP_PHONE_ID: str = os.getenv("WHATSAPP_PHONE_ID", "pega_tu_phone_id_aqui")
+    WHATSAPP_TOKEN: str = os.getenv("WHATSAPP_TOKEN", "")
+    WHATSAPP_PHONE_ID: str = os.getenv("WHATSAPP_PHONE_ID", "")
 
 settings = Settings()
